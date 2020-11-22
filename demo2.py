3668b57c6f7471b08ecd00ddbb57f11d783e914c
Golds-l <57249279+Golds-l@users.noreply.github.com>
2020-11-22 21:25:18 +0800

    Add files via upload

--- demo2.py
+++ demo2.py
@@ -1,38 +1,38 @@
-import os, sys, glob, shutil, json
+import glob
+import json
+
+import datetime
+import numpy as np
+# %pylab inline
+import torch
+from PIL import Image
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = '0'  ##用于指定用哪块GPU
 # import cv2
 
-from PIL import Image
-import numpy as np
-
-from tqdm import tqdm, tqdm_notebook  ##时间进度条模块
-
-# %pylab inline
-import torch
-
 torch.manual_seed(0)  # 限定一个随机种子点，保证每次运行结果相同，方便验证
 
 # # 使用GPU需用用到cuda模块，配合cudnn加速模块一起使用
-# torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.deterministic = True
 # # 用以保证实验的可重复性
-# torch.backends.cudnn.benchmark = True
+torch.backends.cudnn.benchmark = True
 
 import torchvision.models as models
 import torchvision.transforms as transforms
 # import torchvision.datasets as datasets
 import torch.nn as nn
-# import torch.nn.functional as F
 # import torch.optim as optim
 # from torch.autograd import Variable
 from torch.utils.data.dataset import Dataset
 
-train_path = glob.glob('input/train/*.png')
+name_file = "demo2"
+print(name_file)
+
+train_path = glob.glob('input/train_augmentation/*.png')
 # glob函数用于模糊匹配一定格式的文件名
 train_path.sort()
-train_json = json.load(open('input/train.json'))
+train_json = json.load(open('input/augmentation.json'))
 train_label = [train_json[x]['label'] for x in train_json]
-print(len(train_path), len(train_label))
 
 
 class SVHNDataset(Dataset):
@@ -58,26 +58,33 @@ class SVHNDataset(Dataset):
         return len(self.img_path)
 
 
-class SVHN_Model1(nn.Module):
+class SVHN_Model(nn.Module):
     def __init__(self):
-        super(SVHN_Model1, self).__init__()
+        super(SVHN_Model, self).__init__()
         model_conv = models.resnet18(pretrained=True)
         # #加载模型，并设为预训练模式
         model_conv.avgpool = nn.AdaptiveAvgPool2d(1)
         # 自适应平均池化，strides，paddings等参数都自适应好了
+        # model_conv.dropout = nn.Dropout(0.4)
         model_conv = nn.Sequential(*list(model_conv.children())[:-1])
         # 取模型结果
         self.cnn = model_conv
 
         self.fc1 = nn.Linear(512, 11)
+        # nn.Dropout(0.5)
         self.fc2 = nn.Linear(512, 11)
+        # nn.Dropout(0.5)
         self.fc3 = nn.Linear(512, 11)
+        # nn.Dropout(0.5)
         self.fc4 = nn.Linear(512, 11)
+        # nn.Dropout(0.5)
         self.fc5 = nn.Linear(512, 11)
+        # nn.Dropout(0.5)
         # 五个fc层并联，由于图片对应的是一个5位数的值，分别对每一个进行识别，所以5个fc
 
     def forward(self, img):
-        feat = self.cnn(img)
+        feat = \
+            self.cnn(img)
         # print(feat.shape)
         feat = feat.view(feat.shape[0], -1)
         c1 = self.fc1(feat)
@@ -98,7 +105,7 @@ train_loader = torch.utils.data.DataLoader(
                     transforms.ToTensor(),  # 转成一个tensor
                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # 标准化
                 ])),
-    batch_size=100,  # 一个batch的图片数量
+    batch_size=64,  # 一个batch的图片数量
     shuffle=True,
     num_workers=0,  # num_workers为线程数目，之前为10，报错，由于线程数与内核数不匹配，把num_workers改为0成功
 )
@@ -107,7 +114,7 @@ val_path = glob.glob('input/validation/*.png')
 val_path.sort()
 val_json = json.load(open('input/validation.json'))
 val_label = [val_json[x]['label'] for x in val_json]
-print(len(val_path), len(val_label))
+# print(len(val_path), len(val_label))
 
 val_loader = torch.utils.data.DataLoader(
     SVHNDataset(val_path, val_label,
@@ -118,7 +125,7 @@ val_loader = torch.utils.data.DataLoader(
                     transforms.ToTensor(),
                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # 使用resnet18必须标准化到特定
                 ])),
-    batch_size=100,
+    batch_size=40,
     shuffle=False,
     num_workers=0,
 )
@@ -137,10 +144,10 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
         c0, c1, c2, c3, c4 = model(input)
         loss = criterion(c0, target[:, 0]) + \
-            criterion(c1, target[:, 1]) + \
-            criterion(c2, target[:, 2]) + \
-            criterion(c3, target[:, 3]) + \
-            criterion(c4, target[:, 4])
+               criterion(c1, target[:, 1]) + \
+               criterion(c2, target[:, 2]) + \
+               criterion(c3, target[:, 3]) + \
+               criterion(c4, target[:, 4])
         # 计算损失函数的时候是5个FC层的损失相加
 
         optimizer.zero_grad()
@@ -165,10 +172,10 @@ def validate(val_loader, model, criterion):
 
             c0, c1, c2, c3, c4 = model(input)
             loss = criterion(c0, target[:, 0]) + \
-                criterion(c1, target[:, 1]) + \
-                criterion(c2, target[:, 2]) + \
-                criterion(c3, target[:, 3]) + \
-                criterion(c4, target[:, 4])
+                   criterion(c1, target[:, 1]) + \
+                   criterion(c2, target[:, 2]) + \
+                   criterion(c3, target[:, 3]) + \
+                   criterion(c4, target[:, 4])
             # loss /= 6
             val_loss.append(loss.item())
     return np.mean(val_loss)
@@ -215,22 +222,21 @@ def predict(test_loader, model, tta=10):
     return test_pred_tta
 
 
-model = SVHN_Model1()
+model = SVHN_Model()
 criterion = nn.CrossEntropyLoss()  # 交叉熵损失函数
-optimizer = torch.optim.Adam(model.parameters(), 0.001)  # 0.001为学习率
+optimizer = torch.optim.Adam(model.parameters(), 0.0008)  # 0.001为学习率
 best_loss = 1000.0
+time = datetime.datetime.now()
 
 # 是否使用GPU
 use_cuda = True
 if use_cuda:
     model = model.cuda()
 
-for epoch in range(10):
-    print(epoch)
+print('GPU: {0}'.format(use_cuda))
+for epoch in range(20):
     train_loss = train(train_loader, model, criterion, optimizer, epoch)
-    print("t")
     val_loss = validate(val_loader, model, criterion)
-    print("t")
     val_label = [''.join(map(str, x)) for x in val_loader.dataset.img_label]
     val_predict_label = predict(val_loader, model, 1)
     val_predict_label = np.vstack([
@@ -246,10 +252,18 @@ for epoch in range(10):
 
     val_char_acc = np.mean(np.array(val_label_pred) == np.array(val_label))
 
-    print('Epoch: {0}, Train loss: {1} \t Val loss: {2}'.format(epoch, train_loss, val_loss))
-    print('Val Acc', val_char_acc)
+    print('Epoch: {0}  Train loss: {1}  Val loss: {2}  val_acc: {3}'.format(epoch, train_loss, val_loss, val_char_acc))
+    with open("log/{0}.txt".format(time.date()), "a") as fp:
+        fp.write(
+            'Mod: {0} V: {1} Epoch: {2} Train loss: {3}Val loss: {4} Val_acc: {5} time: {6} \n'.format("model2",
+                                                                                                       name_file,
+                                                                                                       epoch,
+                                                                                                       train_loss,
+                                                                                                       val_loss,
+                                                                                                       val_char_acc,
+                                                                                                       time))
     # 记录下验证集精度
     if val_loss < best_loss:
         best_loss = val_loss
-        # print('Find better model in Epoch {0}, saving model.'.format(epoch))
-        torch.save(model.state_dict(), './model2.pt')
+        print('Find better model in Epoch {0}, saving model.'.format(epoch))
+        torch.save(model, 'mod/model2_val acc{0}_{1}.pt'.format(val_char_acc, name_file))
