9985d44ae3f9615a958f14f59e557455f58e227f
Golds-l <57249279+Golds-l@users.noreply.github.com>
2020-11-29 17:07:32 +0800

    Add files via upload

--- cut.py
+++ cut.py
@@ -2,192 +2,191 @@ import json
 
 from PIL import Image
 
-train_json_re = json.load(open("data/validation.json"))
-val_json_re = json.load(open("data/validation.json"))
+json_src = json.load(open("data/validation.json"))
+img_path_src = "data/validation_src"
+img_path_tar = "data/validation"
 
-for i in val_json_re:
+for i in json_src:
     print(i)
-    path_img_re = "data/validation_src/{0}".format(i)
+    path_img_re = "{1}/{0}".format(i, img_path_src)
     img_re = Image.open(path_img_re)
-    if len(val_json_re[i]["label"]) == 1:
-        img1_1 = img_re.crop((val_json_re[i]["left"][0], val_json_re[i]["top"][0],
-                              val_json_re[i]["left"][0] + val_json_re[i]["width"][0],
-                              val_json_re[i]["top"][0] + val_json_re[i]["height"][0]))
+    if len(json_src[i]["label"]) == 1:
+        img1_1 = img_re.crop((json_src[i]["left"][0], json_src[i]["top"][0],
+                              json_src[i]["left"][0] + json_src[i]["width"][0],
+                              json_src[i]["top"][0] + json_src[i]["height"][0]))
         img1_1.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][0], str(val_json_re[i]["label"][0]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][0], str(json_src[i]["label"][0]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-    if len(val_json_re[i]["label"]) == 2:
-        img2_1 = img_re.crop((val_json_re[i]["left"][0], val_json_re[i]["top"][0],
-                              val_json_re[i]["left"][0] + val_json_re[i]["width"][0],
-                              val_json_re[i]["top"][0] + val_json_re[i]["height"][0]))
+    if len(json_src[i]["label"]) == 2:
+        img2_1 = img_re.crop((json_src[i]["left"][0], json_src[i]["top"][0],
+                              json_src[i]["left"][0] + json_src[i]["width"][0],
+                              json_src[i]["top"][0] + json_src[i]["height"][0]))
         img2_1.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][0], str(val_json_re[i]["label"][0]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][0], str(json_src[i]["label"][0]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-        img2_2 = img_re.crop((val_json_re[i]["left"][1], val_json_re[i]["top"][1],
-                              val_json_re[i]["left"][1] + val_json_re[i]["width"][1],
-                              val_json_re[i]["top"][1] + val_json_re[i]["height"][1]))
+        img2_2 = img_re.crop((json_src[i]["left"][1], json_src[i]["top"][1],
+                              json_src[i]["left"][1] + json_src[i]["width"][1],
+                              json_src[i]["top"][1] + json_src[i]["height"][1]))
         img2_2.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][1], str(val_json_re[i]["label"][1]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][1], str(json_src[i]["label"][1]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-    if len(val_json_re[i]["label"]) == 3:
-        img3_1 = img_re.crop((val_json_re[i]["left"][0], val_json_re[i]["top"][0],
-                              val_json_re[i]["left"][0] + val_json_re[i]["width"][0],
-                              val_json_re[i]["top"][0] + val_json_re[i]["height"][0]))
+    if len(json_src[i]["label"]) == 3:
+        img3_1 = img_re.crop((json_src[i]["left"][0], json_src[i]["top"][0],
+                              json_src[i]["left"][0] + json_src[i]["width"][0],
+                              json_src[i]["top"][0] + json_src[i]["height"][0]))
         img3_1.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][0], str(val_json_re[i]["label"][0]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][0], str(json_src[i]["label"][0]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-        img3_2 = img_re.crop((val_json_re[i]["left"][1], val_json_re[i]["top"][1],
-                              val_json_re[i]["left"][1] + val_json_re[i]["width"][1],
-                              val_json_re[i]["top"][1] + val_json_re[i]["height"][1]))
+        img3_2 = img_re.crop((json_src[i]["left"][1], json_src[i]["top"][1],
+                              json_src[i]["left"][1] + json_src[i]["width"][1],
+                              json_src[i]["top"][1] + json_src[i]["height"][1]))
         img3_2.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][1], str(val_json_re[i]["label"][1]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][1], str(json_src[i]["label"][1]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-        img3_3 = img_re.crop((val_json_re[i]["left"][2], val_json_re[i]["top"][2],
-                              val_json_re[i]["left"][2] + val_json_re[i]["width"][2],
-                              val_json_re[i]["top"][2] + val_json_re[i]["height"][2]))
+        img3_3 = img_re.crop((json_src[i]["left"][2], json_src[i]["top"][2],
+                              json_src[i]["left"][2] + json_src[i]["width"][2],
+                              json_src[i]["top"][2] + json_src[i]["height"][2]))
         img3_3.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][2], str(val_json_re[i]["label"][2]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][2], str(json_src[i]["label"][2]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-    if len(val_json_re[i]["label"]) == 4:
-        img4_1 = img_re.crop((val_json_re[i]["left"][0], val_json_re[i]["top"][0],
-                              val_json_re[i]["left"][0] + val_json_re[i]["width"][0],
-                              val_json_re[i]["top"][0] + val_json_re[i]["height"][0]))
+    if len(json_src[i]["label"]) == 4:
+        img4_1 = img_re.crop((json_src[i]["left"][0], json_src[i]["top"][0],
+                              json_src[i]["left"][0] + json_src[i]["width"][0],
+                              json_src[i]["top"][0] + json_src[i]["height"][0]))
         img4_1.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][0], str(val_json_re[i]["label"][0]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][0], str(json_src[i]["label"][0]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-        img4_2 = img_re.crop((val_json_re[i]["left"][1], val_json_re[i]["top"][1],
-                              val_json_re[i]["left"][1] + val_json_re[i]["width"][1],
-                              val_json_re[i]["top"][1] + val_json_re[i]["height"][1]))
+        img4_2 = img_re.crop((json_src[i]["left"][1], json_src[i]["top"][1],
+                              json_src[i]["left"][1] + json_src[i]["width"][1],
+                              json_src[i]["top"][1] + json_src[i]["height"][1]))
         img4_2.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][1], str(val_json_re[i]["label"][1]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][1], str(json_src[i]["label"][1]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-        img4_3 = img_re.crop((val_json_re[i]["left"][2], val_json_re[i]["top"][2],
-                              val_json_re[i]["left"][2] + val_json_re[i]["width"][2],
-                              val_json_re[i]["top"][2] + val_json_re[i]["height"][2]))
+        img4_3 = img_re.crop((json_src[i]["left"][2], json_src[i]["top"][2],
+                              json_src[i]["left"][2] + json_src[i]["width"][2],
+                              json_src[i]["top"][2] + json_src[i]["height"][2]))
         img4_3.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][2], str(val_json_re[i]["label"][2]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][2], str(json_src[i]["label"][2]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-        img4_4 = img_re.crop((val_json_re[i]["left"][3], val_json_re[i]["top"][3],
-                              val_json_re[i]["left"][3] + val_json_re[i]["width"][3],
-                              val_json_re[i]["top"][3] + val_json_re[i]["height"][3]))
+        img4_4 = img_re.crop((json_src[i]["left"][3], json_src[i]["top"][3],
+                              json_src[i]["left"][3] + json_src[i]["width"][3],
+                              json_src[i]["top"][3] + json_src[i]["height"][3]))
         img4_4.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][3], str(val_json_re[i]["label"][3]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][3], str(json_src[i]["label"][3]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-    if len(val_json_re[i]["label"]) == 5:
-        img5_1 = img_re.crop((val_json_re[i]["left"][0], val_json_re[i]["top"][0],
-                              val_json_re[i]["left"][0] + val_json_re[i]["width"][0],
-                              val_json_re[i]["top"][0] + val_json_re[i]["height"][0]))
+    if len(json_src[i]["label"]) == 5:
+        img5_1 = img_re.crop((json_src[i]["left"][0], json_src[i]["top"][0],
+                              json_src[i]["left"][0] + json_src[i]["width"][0],
+                              json_src[i]["top"][0] + json_src[i]["height"][0]))
         img5_1.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][0], str(val_json_re[i]["label"][0]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][0], str(json_src[i]["label"][0]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-        img5_2 = img_re.crop((val_json_re[i]["left"][1], val_json_re[i]["top"][1],
-                              val_json_re[i]["left"][1] + val_json_re[i]["width"][1],
-                              val_json_re[i]["top"][1] + val_json_re[i]["height"][1]))
+        img5_2 = img_re.crop((json_src[i]["left"][1], json_src[i]["top"][1],
+                              json_src[i]["left"][1] + json_src[i]["width"][1],
+                              json_src[i]["top"][1] + json_src[i]["height"][1]))
         img5_2.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][1], str(val_json_re[i]["label"][1]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][1], str(json_src[i]["label"][1]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-        img5_3 = img_re.crop((val_json_re[i]["left"][2], val_json_re[i]["top"][2],
-                              val_json_re[i]["left"][2] + val_json_re[i]["width"][2],
-                              val_json_re[i]["top"][2] + val_json_re[i]["height"][2]))
+        img5_3 = img_re.crop((json_src[i]["left"][2], json_src[i]["top"][2],
+                              json_src[i]["left"][2] + json_src[i]["width"][2],
+                              json_src[i]["top"][2] + json_src[i]["height"][2]))
         img5_3.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][2], str(val_json_re[i]["label"][2]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][2], str(json_src[i]["label"][2]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-        img5_4 = img_re.crop((val_json_re[i]["left"][3], val_json_re[i]["top"][3],
-                              val_json_re[i]["left"][3] + val_json_re[i]["width"][3],
-                              val_json_re[i]["top"][3] + val_json_re[i]["height"][3]))
+        img5_4 = img_re.crop((json_src[i]["left"][3], json_src[i]["top"][3],
+                              json_src[i]["left"][3] + json_src[i]["width"][3],
+                              json_src[i]["top"][3] + json_src[i]["height"][3]))
         img5_4.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][3], str(val_json_re[i]["label"][3]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][3], str(json_src[i]["label"][3]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-        img5_5 = img_re.crop((val_json_re[i]["left"][4], val_json_re[i]["top"][4],
-                              val_json_re[i]["left"][4] + val_json_re[i]["width"][4],
-                              val_json_re[i]["top"][4] + val_json_re[i]["height"][4]))
+        img5_5 = img_re.crop((json_src[i]["left"][4], json_src[i]["top"][4],
+                              json_src[i]["left"][4] + json_src[i]["width"][4],
+                              json_src[i]["top"][4] + json_src[i]["height"][4]))
         img5_5.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][4], str(val_json_re[i]["label"][4]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][4], str(json_src[i]["label"][4]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-    if len(val_json_re[i]["label"]) == 6:
-        img6_1 = img_re.crop((val_json_re[i]["left"][0], val_json_re[i]["top"][0],
-                              val_json_re[i]["left"][0] + val_json_re[i]["width"][0],
-                              val_json_re[i]["top"][0] + val_json_re[i]["height"][0]))
+    if len(json_src[i]["label"]) == 6:
+        img6_1 = img_re.crop((json_src[i]["left"][0], json_src[i]["top"][0],
+                              json_src[i]["left"][0] + json_src[i]["width"][0],
+                              json_src[i]["top"][0] + json_src[i]["height"][0]))
         img6_1.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][0], str(val_json_re[i]["label"][0]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][0], str(json_src[i]["label"][0]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-        img6_2 = img_re.crop((val_json_re[i]["left"][1], val_json_re[i]["top"][1],
-                              val_json_re[i]["left"][1] + val_json_re[i]["width"][1],
-                              val_json_re[i]["top"][1] + val_json_re[i]["height"][1]))
+        img6_2 = img_re.crop((json_src[i]["left"][1], json_src[i]["top"][1],
+                              json_src[i]["left"][1] + json_src[i]["width"][1],
+                              json_src[i]["top"][1] + json_src[i]["height"][1]))
         img6_2.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][1], str(val_json_re[i]["label"][1]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][1], str(json_src[i]["label"][1]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-        img6_3 = img_re.crop((val_json_re[i]["left"][2], val_json_re[i]["top"][2],
-                              val_json_re[i]["left"][2] + val_json_re[i]["width"][2],
-                              val_json_re[i]["top"][2] + val_json_re[i]["height"][2]))
+        img6_3 = img_re.crop((json_src[i]["left"][2], json_src[i]["top"][2],
+                              json_src[i]["left"][2] + json_src[i]["width"][2],
+                              json_src[i]["top"][2] + json_src[i]["height"][2]))
         img6_3.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][2], str(val_json_re[i]["label"][2]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][2], str(json_src[i]["label"][2]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-        img6_4 = img_re.crop((val_json_re[i]["left"][3], val_json_re[i]["top"][3],
-                              val_json_re[i]["left"][3] + val_json_re[i]["width"][3],
-                              val_json_re[i]["top"][3] + val_json_re[i]["height"][3]))
+        img6_4 = img_re.crop((json_src[i]["left"][3], json_src[i]["top"][3],
+                              json_src[i]["left"][3] + json_src[i]["width"][3],
+                              json_src[i]["top"][3] + json_src[i]["height"][3]))
         img6_4.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][3], str(val_json_re[i]["label"][3]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][3], str(json_src[i]["label"][3]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-        img6_5 = img_re.crop((val_json_re[i]["left"][4], val_json_re[i]["top"][4],
-                              val_json_re[i]["left"][4] + val_json_re[i]["width"][4],
-                              val_json_re[i]["top"][4] + val_json_re[i]["height"][4]))
+        img6_5 = img_re.crop((json_src[i]["left"][4], json_src[i]["top"][4],
+                              json_src[i]["left"][4] + json_src[i]["width"][4],
+                              json_src[i]["top"][4] + json_src[i]["height"][4]))
         img6_5.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][4], str(val_json_re[i]["label"][4]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][4], str(json_src[i]["label"][4]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-        img6_6 = img_re.crop((val_json_re[i]["left"][5], val_json_re[i]["top"][5],
-                              val_json_re[i]["left"][5] + val_json_re[i]["width"][5],
-                              val_json_re[i]["top"][5] + val_json_re[i]["height"][5]))
+        img6_6 = img_re.crop((json_src[i]["left"][5], json_src[i]["top"][5],
+                              json_src[i]["left"][5] + json_src[i]["width"][5],
+                              json_src[i]["top"][5] + json_src[i]["height"][5]))
         img6_6.save(
-            "data/validation/{0}/{1}".format(val_json_re[i]["label"][5], str(val_json_re[i]["label"][5]) + i),
+            "{2}/{0}/{1}".format(json_src[i]["label"][5], str(json_src[i]["label"][5]) + i, img_path_tar),
             quality=95,
             subsampling=0)
 
-# val_img_path_src = "data/validation/"
-# val_json_path_src = "data/validation.json"
 #
 #
-# def path_process(path_src, json_path):
+# def path_process(pat,img_path_tarh_src, json_path):
 #     img_path = []
 #     img_json = json.load(open(json_path))
 #     for i in img_json:
